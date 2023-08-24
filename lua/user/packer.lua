000aff1953e97d4b0b937d69c1b53e4c6099037c
osorokin <sorokin.oleg.edu@gmail.com>
2023-08-25 02:54:23 +0300

    Remove unused theme

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -5,18 +5,13 @@ return require('packer').startup(function (use)
     use 'wbthomason/packer.nvim'
 
     use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }})
+
     use 'nvim-treesitter/nvim-treesitter-context'
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
         requires = { {'nvim-lua/plenary.nvim'} },
     }
-
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = 'vim.cmd[[colorscheme rose-pine]]',
-    })
 
     use 'tpope/vim-fugitive'
 

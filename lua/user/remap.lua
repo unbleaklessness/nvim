7e08d94132213d53331c3c40929fe17e707c38c1
osorokin <sorokin.oleg.edu@gmail.com>
2023-10-23 00:48:53 +0300

    Formatting paste

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -6,13 +6,55 @@ vim.api.nvim_set_keymap(
 )
 
 vim.api.nvim_set_keymap("n", "<leader>y", "\"+y", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>y", "\"+y", { noremap = true, silent = true })
-
 vim.api.nvim_set_keymap("n", "<leader>Y", "\"+Y", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("v", "<leader>y", "\"+y", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<leader>Y", "\"+Y", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<leader>p", "\"+p", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>p", "\"+p", { noremap = true, silent = true })
+local function bind_formatting_paste()
+    vim.api.nvim_set_keymap("n", "p", "pmt`[v`]=`t", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "P", "Pmt`[v`]=`t", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<leader>P", "\"+P", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>P", "\"+P", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "p", "pmt`[v`]=`t", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "P", "Pmt`[v`]=`t", { noremap = true, silent = true })
+
+    vim.api.nvim_set_keymap("n", "<leader>p", "\"+pmt`[v`]=`t", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>P", "\"+Pmt`[v`]=`t", { noremap = true, silent = true })
+
+    vim.api.nvim_set_keymap("v", "<leader>p", "\"+pmt`[v`]=`t", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "<leader>P", "\"+Pmt`[v`]=`t", { noremap = true, silent = true })
+end
+
+local function bind_normal_paste()
+    vim.api.nvim_set_keymap("n", "p", "p", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "P", "P", { noremap = true, silent = true })
+
+    vim.api.nvim_set_keymap("v", "p", "p", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "P", "P", { noremap = true, silent = true })
+
+    vim.api.nvim_set_keymap("n", "<leader>p", "\"+p", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>P", "\"+P", { noremap = true, silent = true })
+
+    vim.api.nvim_set_keymap("v", "<leader>p", "\"+p", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "<leader>P", "\"+P", { noremap = true, silent = true })
+end
+
+local function set_formatting_paste(enabled)
+    if enabled then
+        bind_formatting_paste()
+    else
+        bind_normal_paste()
+    end
+end
+
+local function toggle_formatting_paste()
+    vim.g.formatting_paste_enabled = not vim.g.formatting_paste_enabled
+    set_formatting_paste(vim.g.formatting_paste_enabled)
+end
+
+vim.g.formatting_paste_enabled = true
+
+set_formatting_paste(vim.g.formatting_paste_enabled)
+
+vim.keymap.set("n", "<leader>=", toggle_formatting_paste, { noremap = true, silent = true })
+

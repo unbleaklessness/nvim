0ad25f4c0caf7b352899e39b7bbb5e264652fd0e
osorokin <sorokin.oleg.edu@gmail.com>
2023-10-27 03:30:32 +0300

    Commit

--- after/plugin/auto_close.lua
+++ after/plugin/auto_close.lua
@@ -1,1 +1,9 @@
-require("autoclose").setup()
+require("autoclose").setup({
+    options = {
+        disable_command_mode = true,
+        disable_when_touch = true,
+        pair_spaces = true,
+        disabled_filetypes = { "TelescopePrompt" },
+    },
+})
+
